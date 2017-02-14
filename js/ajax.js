936773033d1ed49efa47c07eba4574f40693a477
Ingunn <ingunnrob@gmail.com>
2017-02-14 14:07:06 +0000

    Vesenis - pay no mind

--- js/ajax.js
+++ js/ajax.js
@@ -1,21 +1,32 @@
 //MODB api
 var r = new XMLHttpRequest(); //hluti af javascript
-r.open("GET", 'https://api.themoviedb.org/3/movie/550?api_key=e72b9a3547c05a93b2bd985d1db338fa', true);
+r.open("GET", 'https://api.themoviedb.org/3/discover/movie?api_key=e72b9a3547c05a93b2bd985d1db338fa&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1', true);
 r.onreadystatechange = function () {
   if (r.readyState != 4 || r.status != 200) return;
 //  alert("Success: " + r.responseText);
 	const response = JSON.parse(r.responseText);
+
+	const mostpopular = response.results; // tók út arrey til að fá allar myndirnar en ekki bara eina
+	const mpmovie = new Render (mostpopular);
+//console.log(mostpopular);
+	//const container = document.createElement("div");
+	//const image = document.createElement("img");
+	//const title = document.createElement("h1");
+	//image.src = "http://image.tmdb.org/t/p/original" + response.backdrop_path;
+	//title.innerHTML = response.original_title;
+	//container.append(image);
+	//container.append(title);
+	//document.body.append(container);
 	
+
 };
 r.send();
 
 
 
-
- //kvikmyndir.is api
-
+//kvikmyndir.is api
 var AJAX = new XMLHttpRequest();
-AJAX.open("GET", "http://api.kvikmyndir.is/movies"); // alltaf "GET", - hér skiptir maður út movies fyrir það sem maður vill ná í úr apanum
+AJAX.open("GET", "http://api.kvikmyndir.is/movies", true); // alltaf "GET", - hér skiptir maður út movies fyrir það sem maður vill ná í úr apanum
 AJAX.setRequestHeader("x-access-token","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1OGExYTBjYWQxM2U2YmQ4NzJlZjZkZjIiLCJnbG9iYWxhZG1pbiI6ZmFsc2UsImFkbWluIjpmYWxzZSwiYWN0aXZlIjp0cnVlLCJmdWxsbmFtZSI6IkhyZWZuYSBLcmlzdGJqw7ZybnNkw7N0dGlyIiwiZW1haWwiOiJocmVmbmF0aG9yZXlAZ21haWwuY29tIiwidXNlcm5hbWUiOiJocmVmbmF0aCIsInBhc3N3b3JkIjoiJDJhJDA4JHdrRXFFczZUb2R6cjc4LkQ2dFhvT2VnLm9EWmYuRUljWm5NODd0OERBL2g5YmlVTzVoRWRxIiwiZG9tYWluIjoidmVmc2tvbGkuaXMiLCJtZXNzYWdlIjoic21hbGwgc2Nob29sIHByb2RqZWN0IiwiaWF0IjoxNDg2OTg3NDc5LCJleHAiOjE0ODcwNzM4Nzl9.yyipkgLXOypMm2u9hvZWcR50rwsfFhU9yJgewaBKLMc"); // key númerið sem maður fær
 AJAX.onreadystatechange = function() {
 	if(AJAX.readyState != 4 || AJAX.status != 200) { // 4 - svar frá servernum og fer niður í const response...verður að vera 200 til að fá upplýsingarnar úr apanaum
@@ -24,9 +35,9 @@ AJAX.onreadystatechange = function() {
 	const response = JSON.parse(AJAX.responseText) // const því innihaldið breytist ekki
 	// data = AJAX.responseText;
 	console.log(response);
-}
-AJAX send();
+};
 
+//AJAX send();
 
 
 
@@ -89,6 +100,3 @@ AJAX send();
 //let consoleItem = new MdbAjax();
 //console.log(consoleItem.makeAjaxCall());
 	
-
-
-
