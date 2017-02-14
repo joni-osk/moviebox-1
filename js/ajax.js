b4577f09f60d39c22a709455fc4b0505c394c9ee
Ingunn <ingunnrob@gmail.com>
2017-02-14 10:20:26 +0000

    Smávægilegar breytingar á search

--- js/ajax.js
+++ js/ajax.js
@@ -5,14 +5,7 @@ r.onreadystatechange = function () {
   if (r.readyState != 4 || r.status != 200) return;
 //  alert("Success: " + r.responseText);
 	const response = JSON.parse(r.responseText);
-	const container = document.createElement("div");
-	const image = document.createElement("img");
-	const title = document.createElement("h1");
-	//image.src = "http://image.tmdb.org/t/p/original" + response.backdrop_path;
-	//title.innerHTML = response.original_title;
-	//container.append(image);
-	//container.append(title);
-	//document.body.append(container);
+	
 };
 r.send();
 
--- js/search.js
+++ js/search.js
@@ -16,3 +16,22 @@ $(".rating-slider")
     .slider("float", {
         handle: true
     });
+
+$(".year-slider")
+    .slider({
+				min: 1888,
+				max: 2017,
+				range: true,
+				values: [1888,2017]
+	
+		})
+    .slider("pips", {
+        first: "lable",
+				last: "lable",
+				rest: false,
+				step: 5
+				
+    })
+    .slider("float", {
+        handle: true
+    });
